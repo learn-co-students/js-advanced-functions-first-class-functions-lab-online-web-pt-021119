bf96976a4c0eec0a24727b0d292cb1f8f0d27270
Nataliya R <n.rudenko@gmail.com>
2019-09-13 02:26:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  const returnFirstTwoDrivers = function(drivers) {
     return drivers.slice(0, 2) }; 
 
@@ -20,3 +21,9 @@ const fareTripler = createFareMultiplier(3)
 const selectDifferentDrivers = function (list,fn) {
   return fn(list);
 }
+=======
+ const returnFirstTwoDrivers(driver) = function() {
+    return driver[0],[1] }; 
+
+
+>>>>>>> 3d96f8b46f3690b6ecc81114ccc9244e01add833
